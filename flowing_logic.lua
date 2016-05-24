0d7917df4510c8e28aa07bfba9391c8d8a646051
HybridDog <ovvv@web.de>
2016-05-24 21:32:32 +0200

    cleanup the pipeworks.spigot_check function, fixes #137

--- flowing_logic.lua
+++ flowing_logic.lua
@@ -60,8 +60,8 @@ pipeworks.check_for_inflows = function(pos,node)
 			end
 		end
 	end
-	if newnode then 
-		minetest.add_node(pos,{name=newnode, param2 = node.param2}) 
+	if newnode then
+		minetest.add_node(pos,{name=newnode, param2 = node.param2})
 		minetest.get_meta(pos):set_string("source",minetest.pos_to_string(source))
 	end
 end
@@ -75,45 +75,46 @@ pipeworks.check_sources = function(pos,node)
 		newnode = string.gsub(node.name,"loaded","empty")
 	end
 
-	if newnode then 
-		minetest.add_node(pos,{name=newnode, param2 = node.param2}) 
+	if newnode then
+		minetest.add_node(pos,{name=newnode, param2 = node.param2})
 		minetest.get_meta(pos):set_string("source","")
 	end
 end
 
 pipeworks.spigot_check = function(pos, node)
 	local belowname = minetest.get_node({x=pos.x,y=pos.y-1,z=pos.z}).name
-	if belowname and (belowname == "air" or belowname == "default:water_flowing" or belowname == "default:water_source") then 
-		local spigotname = minetest.get_node(pos).name
-		local fdir=node.param2 % 4
-		local check = {
-			{x=pos.x,y=pos.y,z=pos.z+1},
-			{x=pos.x+1,y=pos.y,z=pos.z},
-			{x=pos.x,y=pos.y,z=pos.z-1},
-			{x=pos.x-1,y=pos.y,z=pos.z}
-		}
-		local near_node = minetest.get_node(check[fdir+1])
-		if near_node and string.find(near_node.name, "_loaded") then
-			if spigotname and spigotname == "pipeworks:spigot" then
-				minetest.add_node(pos,{name = "pipeworks:spigot_pouring", param2 = fdir})
-				if finitewater or belowname ~= "default:water_source" then
-					minetest.add_node({x=pos.x,y=pos.y-1,z=pos.z},{name = "default:water_source"})
-				end
+	if belowname ~= "air"
+	and belowname ~= "default:water_flowing"
+	and belowname ~= "default:water_source" then
+		return
+	end
+	local addend = math.sign(1.5 - node.param2)
+	local coord = node.param2 % 2 == 0 and "z" or "x"
+	pos[coord] = pos[coord] + addend
+	local nearname = minetest.get_node(pos).name
+	pos[coord] = pos[coord] - addend
+	if string.find(nearname, "_loaded") then
+		if node.name == "pipeworks:spigot" then
+			node.name = "pipeworks:spigot_pouring"
+			minetest.add_node(pos, node)
+			if finitewater
+			or belowname ~= "default:water_source" then
+				minetest.add_node({x=pos.x,y=pos.y-1,z=pos.z}, {name = "default:water_source"})
 			end
-		else
-			if spigotname == "pipeworks:spigot_pouring" then
-				minetest.add_node({x=pos.x,y=pos.y,z=pos.z},{name = "pipeworks:spigot", param2 = fdir})
-				if belowname == "default:water_source" and not finitewater then
-					minetest.remove_node({x=pos.x,y=pos.y-1,z=pos.z})
-				end
-			end
+		end
+	elseif node.name == "pipeworks:spigot_pouring" then
+		node.name = "pipeworks:spigot"
+		minetest.add_node(pos, node)
+		if belowname == "default:water_source"
+		and not finitewater then
+			minetest.remove_node({x=pos.x,y=pos.y-1,z=pos.z})
 		end
 	end
 end
 
 pipeworks.fountainhead_check = function(pos, node)
 	local abovename = minetest.get_node({x=pos.x,y=pos.y+1,z=pos.z}).name
-	if abovename and (abovename == "air" or abovename == "default:water_flowing" or abovename == "default:water_source") then 
+	if abovename and (abovename == "air" or abovename == "default:water_flowing" or abovename == "default:water_source") then
 		local fountainhead_name = minetest.get_node(pos).name
 		local near_node = minetest.get_node({x=pos.x,y=pos.y-1,z=pos.z})
 		if near_node and string.find(near_node.name, "_loaded") then
